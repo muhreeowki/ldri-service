dc7a864e3ed821c7987db002789341c101cd6399
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-11-28 17:20:14 +0300

    Added message storage initialization

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func main() {
+	// Connect to MessageQueue Server
 	queue, err := NewMessageQueue(os.Getenv("AMQP_URI"))
 	failOnError(err, "Failed to connect to RabbitMQ")
+	defer queue.Close()
+	log.Printf("Successfully Connected to RabbitMQ: %v", queue)
 
-	log.Printf("Successfully Connected to RabbitMQ: %+v", queue)
+	// Connect to Storage Server
+	store, err := NewMongoStore(os.Getenv("MONGO_URI"))
+	failOnError(err, "Failed to connect to MongoDB")
+	defer store.Close()
+	log.Printf("Successfully Connected to MongoDB: %v", store)
 
 	server := NewServer(":8080", nil, nil)
 	log.Fatal(server.ListenAndServer())
--- store.go
+++ store.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type Store interface{}
 
-type MongoStore struct{}
+type MongoStore struct {
+	client *mongo.Client
+	db     *mongo.Database
+}
+
+func NewMongoStore(conUri string) (*MongoStore, error) {
+	// Connect to MongoDB
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(conUri))
+	if err != nil {
+		return nil, err
+	}
+	// Check if connection
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	db := client.Database("LDRI")
+
+	return &MongoStore{
+		client,
+		db,
+	}, nil
+}
+
+func (s *MongoStore) Close() {
+	if err := s.client.Disconnect(context.TODO()); err != nil {
+		failOnError(err, "failed to close mongo connection")
+	}
+}
