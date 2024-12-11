8cc6afb322365057722b81d90b4f91145970e9fc
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2024-12-11 14:29:56 +0300

    Set up scaffolding for backend server

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"log"
-	"os"
 )
 
 func main() {
 	// Connect to MessageQueue Server
-	queue, err := NewMessageQueue(os.Getenv("AMQP_URI"))
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer queue.Close()
-	log.Printf("Successfully Connected to RabbitMQ: %v", queue)
+	// queue, err := NewMessageQueue(os.Getenv("AMQP_URI"))
+	// failOnError(err, "Failed to connect to RabbitMQ")
+	// defer queue.Close()
+	// log.Printf("Successfully Connected to RabbitMQ: %v", queue)
 
 	// Connect to Storage Server
-	store, err := NewMongoStore(os.Getenv("MONGO_URI"))
-	failOnError(err, "Failed to connect to MongoDB")
-	defer store.Close()
-	log.Printf("Successfully Connected to MongoDB: %v", store)
+	// store, err := NewMongoStore(os.Getenv("MONGO_URI"))
+	// failOnError(err, "Failed to connect to MongoDB")
+	// defer store.Close()
+	// log.Printf("Successfully Connected to MongoDB: %v", store)
 
+	// Start the server
 	server := NewServer(":8080", nil, nil)
 	log.Fatal(server.ListenAndServer())
 }
--- server.go
+++ server.go
@@ -21,17 +21,41 @@ func NewServer(listenAddress string, store *Store, queue *RabbitQueue) *HTTPServ
 
 func (s *HTTPServer) ListenAndServer() error {
 	router := http.NewServeMux()
-	router.HandleFunc("/health", s.makeHandlerFunc(s.healthCheckHandler))
+	router.HandleFunc("GET /health", makeHandlerFunc(s.healthCheckHandler))
+	router.HandleFunc("POST /login", makeHandlerFunc(s.loginHandler))
+	router.HandleFunc("POST /signup", makeHandlerFunc(s.signUpHandler))
+	router.HandleFunc("GET /data", makeHandlerFunc(s.getDataHandler))
+	log.Printf("Listening on %s...\n", s.listenAddress)
 	return http.ListenAndServe(s.listenAddress, router)
 }
 
+// ----------------- HANDLERS -----------------
+
 func (s *HTTPServer) healthCheckHandler(w http.ResponseWriter, r *http.Request) error {
-	return WriteJSON(w, http.StatusOK, map[string]string{"status": "ok"})
+	log.Printf("Health check request")
+	return WriteJSON(w, http.StatusOK, map[string]string{"message": "ok"})
 }
+
+func (s *HTTPServer) loginHandler(w http.ResponseWriter, r *http.Request) error {
+	log.Printf("Login request")
+	return WriteJSON(w, http.StatusOK, map[string]string{"message": "logged in"})
+}
+
+func (s *HTTPServer) signUpHandler(w http.ResponseWriter, r *http.Request) error {
+	log.Printf("Sign up request")
+	return WriteJSON(w, http.StatusOK, map[string]string{"message": "signed up"})
+}
+
+func (s *HTTPServer) getDataHandler(w http.ResponseWriter, r *http.Request) error {
+	log.Printf("Get data request")
+	return WriteJSON(w, http.StatusOK, map[string]string{"data": "some odk data"})
+}
+
+// ----------------- UTILS -----------------
 
 type ServerHandler func(w http.ResponseWriter, r *http.Request) error
 
-func (s *HTTPServer) makeHandlerFunc(h ServerHandler) http.HandlerFunc {
+func makeHandlerFunc(h ServerHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := h(w, r); err != nil {
 			log.Printf("Error occured: %v", err)
